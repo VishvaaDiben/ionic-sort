a9450b1b811eede5950fb3bf7e21d69150fcbcbe
unknown <dibenvishvaa@gmail.com>
2020-06-25 07:14:07 +0800

    added barchart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,46 +1,90 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { DataService } from "../services/data.service";
-import * as moment from "moment";
+// import * as moment from "moment";
+import { Chart } from "chart.js";
 
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage implements OnInit {
+  @ViewChild("barCanvas", { static: true }) barCanvas;
+
+  barChart: any;
   public searchTerm: string = "";
   public items: any;
   public things: any;
 
-  // todate: any;
-  // today: any;
-  // selectedDate: any;
-  // modifiedData: any;
-  
+  constructor(private dataService: DataService) {}
 
-  constructor(private dataService: DataService) {
-    // this.today = new Date().toISOString();
-  }
-  
   ngOnInit() {
     this.setFilteredItems();
-    this.checkDataForChart();
+    this.barChartMethod();
   }
 
   setFilteredItems() {
     this.items = this.dataService.filterItems(this.searchTerm);
-    const filterDateAndTotal= this.items.sort(function (a, b) {
-      return  b.total_visits - a.total_visits
-    }).slice(0,5);
+    const filterDateAndTotal = this.items
+      .sort(function (a, b) {
+        return b.total_visits - a.total_visits;
+      })
+      .slice(0, 5);
     this.things = filterDateAndTotal;
   }
 
-  checkDataForChart(){
-    console.log("check chart data",this.things)
+  barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: [
+          this.things[0].Island_name,
+          this.things[1].Island_name,
+          this.things[2].Island_name,
+          this.things[3].Island_name,
+          this.things[4].Island_name,
+        ],
+        datasets: [
+          {
+            label: "# Total visit based on Date selected",
+            data: [
+              this.things[0].total_visits,
+              this.things[1].total_visits,
+              this.things[2].total_visits,
+              this.things[3].total_visits,
+              this.things[4].total_visits,
+            ],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
   }
-
-  // parseDate() {
-  //   this.todate = moment(this.selectedDate).format("DD/MM/YYYY");
-  // }
-
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,388 +1,386 @@
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase } from "angularfire2/database";
+import { Chart } from "chart.js";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
   public items: any = [];
+  public chartData: any = [];
 
   constructor(public data: AngularFireDatabase) {
     this.getDataFromFireBase();
-// subscribe data not returning data to global variable, so the first app loads with static.
-// can try remove this.items below, and load app. Then search will populate back data from subscribed database.
-    this.items = 
-    [
+    // subscribe data not returning data to global variable, so the first app loads with static.
+    // can try remove this.items below, and load app. Then search will populate back data from subscribed database.
+    this.items = [
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 4690
+        Island_name: "Pulau Manukan",
+        Visitor_date: "6/3/2017",
+        total_visits: 4690,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 4454
+        Island_name: "Pulau Tioman",
+        Visitor_date: "6/3/2017",
+        total_visits: 4454,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 4070
+        Island_name: "Pulau Sapi",
+        Visitor_date: "4/3/2017",
+        total_visits: 4070,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 3687
+        Island_name: "Pulau Redang",
+        Visitor_date: "3/3/2017",
+        total_visits: 3687,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 3156
+        Island_name: "Pulau Dayang",
+        Visitor_date: "1/3/2017",
+        total_visits: 3156,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2880
+        Island_name: "Pulau Redang",
+        Visitor_date: "7/3/2017",
+        total_visits: 2880,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2793
+        Island_name: "Pulau Mabul",
+        Visitor_date: "7/3/2017",
+        total_visits: 2793,
       },
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2785
+        Island_name: "Pulau Manukan",
+        Visitor_date: "7/3/2017",
+        total_visits: 2785,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 2758
+        Island_name: "Pulau Sapi",
+        Visitor_date: "6/3/2017",
+        total_visits: 2758,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2710
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "7/3/2017",
+        total_visits: 2710,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2677
+        Island_name: "Pulau Sapi",
+        Visitor_date: "7/3/2017",
+        total_visits: 2677,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2645
+        Island_name: "Pulau Kapas",
+        Visitor_date: "7/3/2017",
+        total_visits: 2645,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 2390
+        Island_name: "Pulau Redang",
+        Visitor_date: "5/3/2017",
+        total_visits: 2390,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2386
+        Island_name: "Pulau Tioman",
+        Visitor_date: "7/3/2017",
+        total_visits: 2386,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 2374
+        Island_name: "Pulau Kapas",
+        Visitor_date: "4/3/2017",
+        total_visits: 2374,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 2295
+        Island_name: "Pulau Cenang",
+        Visitor_date: "5/3/2017",
+        total_visits: 2295,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 2282
+        Island_name: "Pulau Cenang",
+        Visitor_date: "6/3/2017",
+        total_visits: 2282,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 2263
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "5/3/2017",
+        total_visits: 2263,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 2223
+        Island_name: "Pulau Mabul",
+        Visitor_date: "6/3/2017",
+        total_visits: 2223,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 2164
+        Island_name: "Pulau Cenang",
+        Visitor_date: "3/3/2017",
+        total_visits: 2164,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 2108
+        Island_name: "Pulau Kapas",
+        Visitor_date: "3/3/2017",
+        total_visits: 2108,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 2103
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "4/3/2017",
+        total_visits: 2103,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 2090
+        Island_name: "Pulau Tioman",
+        Visitor_date: "5/3/2017",
+        total_visits: 2090,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 2046
+        Island_name: "Pulau Cenang",
+        Visitor_date: "7/3/2017",
+        total_visits: 2046,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 2030
+        Island_name: "Pulau Dayang",
+        Visitor_date: "4/3/2017",
+        total_visits: 2030,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 2011
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "3/3/2017",
+        total_visits: 2011,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 1986
+        Island_name: "Pulau Cenang",
+        Visitor_date: "4/3/2017",
+        total_visits: 1986,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1972
+        Island_name: "Sinar Harian",
+        Visitor_date: "2/3/2017",
+        total_visits: 1972,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 1960
+        Island_name: "Sinar Harian",
+        Visitor_date: "3/3/2017",
+        total_visits: 1960,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1928
+        Island_name: "Pulau Dayang",
+        Visitor_date: "2/3/2017",
+        total_visits: 1928,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 1927
+        Island_name: "Pulau Mabul",
+        Visitor_date: "4/3/2017",
+        total_visits: 1927,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1920
+        Island_name: "Pulau Mabul",
+        Visitor_date: "2/3/2017",
+        total_visits: 1920,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 1909
+        Island_name: "Sinar Harian",
+        Visitor_date: "6/3/2017",
+        total_visits: 1909,
       },
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1903
+        Island_name: "Pulau Manukan",
+        Visitor_date: "1/3/2017",
+        total_visits: 1903,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 1896
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "6/3/2017",
+        total_visits: 1896,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 1834
+        Island_name: "Pulau Dayang",
+        Visitor_date: "5/3/2017",
+        total_visits: 1834,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 1827
+        Island_name: "Pulau Dayang",
+        Visitor_date: "7/3/2017",
+        total_visits: 1827,
       },
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1764
+        Island_name: "Pulau Manukan",
+        Visitor_date: "2/3/2017",
+        total_visits: 1764,
       },
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 1734
+        Island_name: "Pulau Manukan",
+        Visitor_date: "4/3/2017",
+        total_visits: 1734,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 1707
+        Island_name: "Pulau Kapas",
+        Visitor_date: "6/3/2017",
+        total_visits: 1707,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1706
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "1/3/2017",
+        total_visits: 1706,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1689
+        Island_name: "Pulau Kapas",
+        Visitor_date: "1/3/2017",
+        total_visits: 1689,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 1684
+        Island_name: "Pulau Redang",
+        Visitor_date: "4/3/2017",
+        total_visits: 1684,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1664
+        Island_name: "Pulau Tioman",
+        Visitor_date: "1/3/2017",
+        total_visits: 1664,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1645
+        Island_name: "Pulau Tioman",
+        Visitor_date: "2/3/2017",
+        total_visits: 1645,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 1634
+        Island_name: "Pulau Tioman",
+        Visitor_date: "4/3/2017",
+        total_visits: 1634,
       },
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 1630
+        Island_name: "Pulau Manukan",
+        Visitor_date: "5/3/2017",
+        total_visits: 1630,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 1564
+        Island_name: "Sinar Harian",
+        Visitor_date: "5/3/2017",
+        total_visits: 1564,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1541
+        Island_name: "Pulau Cenang",
+        Visitor_date: "1/3/2017",
+        total_visits: 1541,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1447
+        Island_name: "Pulau Kapas",
+        Visitor_date: "2/3/2017",
+        total_visits: 1447,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 1400
+        Island_name: "Pulau Sapi",
+        Visitor_date: "3/3/2017",
+        total_visits: 1400,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1347
+        Island_name: "Pulau Sapi",
+        Visitor_date: "1/3/2017",
+        total_visits: 1347,
       },
       {
-        "Island_name": "Pulau Tioman",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 1324
+        Island_name: "Pulau Tioman",
+        Visitor_date: "3/3/2017",
+        total_visits: 1324,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "7/3/2017",
-        "total_visits": 1302
+        Island_name: "Sinar Harian",
+        Visitor_date: "7/3/2017",
+        total_visits: 1302,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 1254
+        Island_name: "Pulau Dayang",
+        Visitor_date: "6/3/2017",
+        total_visits: 1254,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "6/3/2017",
-        "total_visits": 1238
+        Island_name: "Pulau Redang",
+        Visitor_date: "6/3/2017",
+        total_visits: 1238,
       },
       {
-        "Island_name": "Pulau Sipadan",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 1225
+        Island_name: "Pulau Sipadan",
+        Visitor_date: "2/3/2017",
+        total_visits: 1225,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 1149
+        Island_name: "Pulau Redang",
+        Visitor_date: "1/3/2017",
+        total_visits: 1149,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 1119
+        Island_name: "Pulau Mabul",
+        Visitor_date: "5/3/2017",
+        total_visits: 1119,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "4/3/2017",
-        "total_visits": 1063
+        Island_name: "Sinar Harian",
+        Visitor_date: "4/3/2017",
+        total_visits: 1063,
       },
       {
-        "Island_name": "Pulau Manukan",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 1031
+        Island_name: "Pulau Manukan",
+        Visitor_date: "3/3/2017",
+        total_visits: 1031,
       },
       {
-        "Island_name": "Pulau Dayang",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 1015
+        Island_name: "Pulau Dayang",
+        Visitor_date: "3/3/2017",
+        total_visits: 1015,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "3/3/2017",
-        "total_visits": 959
+        Island_name: "Pulau Mabul",
+        Visitor_date: "3/3/2017",
+        total_visits: 959,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 943
+        Island_name: "Pulau Sapi",
+        Visitor_date: "2/3/2017",
+        total_visits: 943,
       },
       {
-        "Island_name": "Pulau Cenang",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 911
+        Island_name: "Pulau Cenang",
+        Visitor_date: "2/3/2017",
+        total_visits: 911,
       },
       {
-        "Island_name": "Sinar Harian",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 823
+        Island_name: "Sinar Harian",
+        Visitor_date: "1/3/2017",
+        total_visits: 823,
       },
       {
-        "Island_name": "Pulau Sapi",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 740
+        Island_name: "Pulau Sapi",
+        Visitor_date: "5/3/2017",
+        total_visits: 740,
       },
       {
-        "Island_name": "Pulau Redang",
-        "Visitor_date": "2/3/2017",
-        "total_visits": 575
+        Island_name: "Pulau Redang",
+        Visitor_date: "2/3/2017",
+        total_visits: 575,
       },
       {
-        "Island_name": "Pulau Mabul",
-        "Visitor_date": "1/3/2017",
-        "total_visits": 500
+        Island_name: "Pulau Mabul",
+        Visitor_date: "1/3/2017",
+        total_visits: 500,
       },
       {
-        "Island_name": "Pulau Kapas",
-        "Visitor_date": "5/3/2017",
-        "total_visits": 1167
-      }
-     ]
-
+        Island_name: "Pulau Kapas",
+        Visitor_date: "5/3/2017",
+        total_visits: 1167,
+      },
+    ];
   }
-
-
 
   getDataFromFireBase() {
     this.data
       .list("/")
       .valueChanges()
-      .subscribe((fireBaseData:any) => {
-        this.items = fireBaseData;
-      },
-      ( error: any) => {
-        console.log(error);
-      });
-      return this.items
+      .subscribe(
+        (fireBaseData: any) => {
+          this.items = fireBaseData;
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+    return this.items;
   }
-
-
 
   filterItems(searchTerm) {
     return this.items.filter((item) => {
@@ -392,4 +390,3 @@ export class DataService {
     });
   }
 }
-
