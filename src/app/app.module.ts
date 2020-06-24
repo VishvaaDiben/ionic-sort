f2cb43fd0182c9779072d2a1729aad6601426360
unknown <dibenvishvaa@gmail.com>
2020-06-24 23:28:08 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+import { HttpClientModule } from '@angular/common/http';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDMwQ3gEmm0VuRfVqnsO6LnHaW7zlRKOsU",
+  authDomain: "tmrd-c4133.firebaseapp.com",
+  databaseURL: "https://tmrd-c4133.firebaseio.com",
+  projectId: "tmrd-c4133",
+  storageBucket: "tmrd-c4133.appspot.com",
+  messagingSenderId: "685877733324",
+  appId: "1:685877733324:web:cf342ba6ac102582e01f69",
+  measurementId: "G-F60NWN6Q90"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AngularFireDatabaseModule, AngularFireModule, AngularFireModule.initializeApp(firebaseConfig), IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../services/data.service";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public searchTerm: string = "";
+  public items: any;
+  public things: any;
 
-  constructor() {}
+  // todate: any;
+  // today: any;
+  // selectedDate: any;
+  // modifiedData: any;
+  
+
+  constructor(private dataService: DataService) {
+    // this.today = new Date().toISOString();
+  }
+  
+  ngOnInit() {
+    this.setFilteredItems();
+    this.checkDataForChart();
+  }
+
+  setFilteredItems() {
+    this.items = this.dataService.filterItems(this.searchTerm);
+    const filterDateAndTotal= this.items.sort(function (a, b) {
+      return  b.total_visits - a.total_visits
+    }).slice(0,5);
+    this.things = filterDateAndTotal;
+  }
+
+  checkDataForChart(){
+    console.log("check chart data",this.things)
+  }
+
+  // parseDate() {
+  //   this.todate = moment(this.selectedDate).format("DD/MM/YYYY");
+  // }
 
 }
